684f7de737dd833b32bb7a03dd375561dfcbc20e
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-03-19 16:52:29 +0200

    Added PromoCode and Review models

---
+++ FoodDeliveryWebApp/Models/PromoCode.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodDeliveryWebApp.Models
+{
+    public class PromoCode : BaseModel
+    {
+        [Range(0,1)]
+        public double Discount { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        //public ICollection<Category> AppliedTo { get; set; } = new List<Category>;
+    }
+}
---
+++ FoodDeliveryWebApp/Models/Review.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodDeliveryWebApp.Models
+{
+    public class Review : BaseModel
+    {
+        [Range(1,5)]
+        public int Rate { get; set; }
+
+        [MaxLength(100)]
+        public string UserReview { get; set; } = string.Empty;
+    }
+}
