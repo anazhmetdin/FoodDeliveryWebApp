897792ffa6d92a10090070de71c7af8c53a824c3
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-03-19 22:05:33 +0200

    added CustomerHomeController

---
+++ FoodDeliveryWebApp/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using FoodDeliveryWebApp.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliveryWebApp.Areas.Customer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICustomerHomeRepo _customerHomeRepo;
+
+        public HomeController(ICustomerHomeRepo customerHomeRepo)
+        {
+            _customerHomeRepo = customerHomeRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_customerHomeRepo.GetSellers());
+        }
+
+        [HttpPost]
+        public IActionResult Index(int id)
+        {
+            return RedirectToAction("Index", "Seller", new { id });
+        }
+    }
+}
