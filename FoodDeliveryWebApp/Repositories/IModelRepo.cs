f2ec0f042c7d54e61923d4449f57dc29ca02758d
anazhmetdin <anazhmetdin@gmail.com>
2023-03-19 17:48:00 +0200

    Add Generic Repositories

---
+++ FoodDeliveryWebApp/Repositories/IModelRepo.cs
@@ -0,0 +1,14 @@
+﻿using FoodDeliveryWebApp.Models;
+
+namespace FoodDeliveryWebApp.Repositories
+{
+    public interface IModelRepo<T> where T : BaseModel
+    {
+        public List<T> GetAll();
+        public T? GetById(int? id);
+        public bool TryInsert(T t);
+        public bool TryUpdate(T t);
+        public bool TryDelete(int? id);
+        public List<T> Where(Func<T, bool> lambda);
+    }
+}
