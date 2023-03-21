714e0aa79dc9b047f6bdd7448cd054bbbf4770f8
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-03-22 00:22:16 +0200

    made registration with roles

--- FoodDeliveryWebApp/Program.cs
+++ FoodDeliveryWebApp/Program.cs
@@ -4,6 +4,7 @@ using FoodDeliveryWebApp.Data;
 using FoodDeliveryWebApp.Areas.Identity.Data;
 using FoodDeliveryWebApp.Contracts;
 using FoodDeliveryWebApp.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace FoodDeliveryWebApp
 {
@@ -13,18 +14,63 @@ namespace FoodDeliveryWebApp
         {
             var builder = WebApplication.CreateBuilder(args);
             var connectionString = builder.Configuration.GetConnectionString("FoodDeliveryWebAppContextConnection") ?? throw new InvalidOperationException("Connection string 'FoodDeliveryWebAppContextConnection' not found.");
-
+          
+            #region Services
             builder.Services.AddDbContext<FoodDeliveryWebAppContext>(options => options.UseSqlServer(connectionString));
 
-            builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<FoodDeliveryWebAppContext>();
+            #region Authentication Services
+            //builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<FoodDeliveryWebAppContext>();
+            
+            builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<FoodDeliveryWebAppContext>().AddDefaultTokenProviders();
+            
+            builder.Services.Configure<DataProtectionTokenProviderOptions>(opts => opts.TokenLifespan = TimeSpan.FromHours(10));
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequiredUniqueChars = 1;
 
-            #region Register repository services
-            builder.Services.AddScoped<ICustomerHomeRepo, CustomerHomeRepo>();
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+            });
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+            
+            builder.Services.AddAuthentication();
+            
+            builder.Services.AddAuthorization();
             #endregion
 
+            #region Repository Services
+            builder.Services.AddScoped<ICustomerHomeRepo, CustomerHomeRepo>();
+            #endregion
+            
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddControllersWithViews();
+            #endregion
+            
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -37,11 +83,15 @@ namespace FoodDeliveryWebApp
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
 
-            app.UseAuthorization();
+            {
+                // DON't CHANGE THEIR ORDER
+                app.UseAuthentication();
+                app.UseAuthorization();
+            }
 
+            app.MapRazorPages();
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
