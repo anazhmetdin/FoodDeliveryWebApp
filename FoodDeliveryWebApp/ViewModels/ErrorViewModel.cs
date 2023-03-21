20735ff3730184a17a38d1f6cee519898dedc3c8
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-03-22 00:21:43 +0200

    separated viewmodels from models

---
+++ FoodDeliveryWebApp/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodDeliveryWebApp.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
