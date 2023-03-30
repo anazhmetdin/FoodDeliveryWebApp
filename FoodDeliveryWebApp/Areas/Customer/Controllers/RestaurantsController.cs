4d75b3cdc6a2b0b8d477ce5ab240df293380e655
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-03-30 02:59:32 +0200

    made customer home with search and filter

--- FoodDeliveryWebApp/Areas/Customer/Controllers/RestaurantsController.cs
+++ FoodDeliveryWebApp/Areas/Customer/Controllers/RestaurantsController.cs
@@ -1,6 +1,10 @@
 ﻿using FoodDeliveryWebApp.Contracts;
+using FoodDeliveryWebApp.Models;
+using FoodDeliveryWebApp.Models.Enums;
+using FoodDeliveryWebApp.Repositories;
 using FoodDeliveryWebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 
 namespace FoodDeliveryWebApp.Areas.Customer.Controllers
 {
@@ -8,15 +12,38 @@ namespace FoodDeliveryWebApp.Areas.Customer.Controllers
     public class RestaurantsController : Controller
     {
         private readonly ICustomerRestaurantsRepo _customerRestaurantRepo;
+        private readonly IModelRepo<Category> _categoryRepo;
 
-        public RestaurantsController(ICustomerRestaurantsRepo customerRestaurantRepo)
+        public RestaurantsController(ICustomerRestaurantsRepo customerRestaurantRepo, IModelRepo<Category> categoryRepo)
         {
             _customerRestaurantRepo = customerRestaurantRepo;
+            _categoryRepo = categoryRepo;
         }
 
         public IActionResult Index()
         {
-            return View(_customerRestaurantRepo.GetSellers());
+            ICollection<SellerViewModel> sellers;
+            if (Request.Query.Count > 0)
+            {
+                if(Request.Query.ContainsKey($"search"))
+                {
+                    ViewBag.Categories = _categoryRepo.GetAll().Select(c => (c.Name, c.Id, false)).ToList();
+                    sellers = _customerRestaurantRepo.GetSellersSearched(Request.Query["search"][0]).ToList();
+                }
+                else
+                {
+                    var cats = _categoryRepo.GetAll().Where(c => Request.Query.ContainsKey($"{c.Id}")).ToList();
+                    sellers = _customerRestaurantRepo.GetSellersFiltered(cats);
+                    ViewBag.Categories = _categoryRepo.GetAll().Select(c => (c.Name, c.Id, Request.Query.ContainsKey($"{c.Id}"))).ToList();
+                }
+            }
+            else
+            {
+                ViewBag.Categories = _categoryRepo.GetAll().Select(c => (c.Name, c.Id, false)).ToList();
+                sellers = _customerRestaurantRepo.GetSellers().ToList();
+            }
+
+            return View(sellers);
         }
 
         [HttpPost]
@@ -24,7 +51,6 @@ namespace FoodDeliveryWebApp.Areas.Customer.Controllers
         {
             return RedirectToAction("Restaurant", new { id });
         }
-
 
         [HttpGet]
         public IActionResult Restaurant(string id)
@@ -39,6 +65,5 @@ namespace FoodDeliveryWebApp.Areas.Customer.Controllers
             Console.WriteLine(items.Count);
             return View();
         }
-
     }
 }
